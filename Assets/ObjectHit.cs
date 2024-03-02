c65ba539c905bb216dae21b6ee34866f5d7423aa
GideoN <quanhong468@gmail.com>
2024-03-02 11:10:40 +0700

    Color wall change when player bump

---
+++ Assets/ObjectHit.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectHit : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision other)
+    {
+        Debug.Log("Bumped into a wall");
+        GetComponent<MeshRenderer>().material.color = Color.red;
+    }
+}
