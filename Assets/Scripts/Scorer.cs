7fab767331c4685e19a0e72989fc2a7d72c48835
GideoN <quanhong468@gmail.com>
2024-03-06 11:27:17 +0700

    Prepare Prefabs

--- Assets/Scripts/Scorer.cs
+++ Assets/Scripts/Scorer.cs
@@ -6,9 +6,13 @@ using UnityEngine;
 public class Scorer : MonoBehaviour
 {
     private int hits = 0;
+
     private void OnCollisionEnter(Collision other)
     {
-        hits++;
-        Debug.Log("You've bumped into a thing this many times: " + hits);
+        if (other.gameObject.tag != "Hit")
+        {
+            hits++;
+            Debug.Log("You've bumped into a thing this many times: " + hits);
+        }
     }
 }
